ff4a22f8825234260a0af93b6ada22513daf9b14
Ruby <nicolas.da-silva@epitech.eu>
2022-06-20 16:31:51 +0200

    #29 : Initialize obboard project

---
+++ Embarqué/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+void setup() {
+// write your initialization code here
+    Serial.begin(9600);
+}
+
+void loop() {
+// write your code here
+    Serial.println("Hello World!");
+    delay(1000);
+}
