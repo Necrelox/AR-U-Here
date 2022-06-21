6b20c56034f68b22dd652a3034e9698658cfa082
Ruby <nicolas.da-silva@epitech.eu>
2022-06-21 12:20:46 +0200

    #29: init base

---
+++ Embarqué/include/recognition.hpp
@@ -0,0 +1,17 @@
+/*
+** recognition.hpp for Embarqué in /home/ruby/Documents/Scolaire/ar-u-here/AR-U-Here/Embarqué/include
+**
+** Made by Ruby
+** Login   <necrelox@gmail.com>
+**
+** Started on  Mon Jun 20 22:42:41 2022 Ruby
+** Last update Mon Jun 20 22:42:41 2022 Ruby
+*/
+
+#ifndef RECOGNITION_HPP_
+# define RECOGNITION_HPP_
+
+#include <HUSKYLENS.h>
+#include "SoftwareSerial.h"
+
+#endif /* !RECOGNITION_HPP_ */
