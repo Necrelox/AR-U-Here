1bef5627801196b808279c525db8ce5ce84fc554
Ruby <nicolas.da-silva@epitech.eu>
2022-07-04 12:19:13 +0200

    #46 : remove old enum files

--- Api/src/server/controller/utils/enum/messageError.ts
+++
@@ -1,4 +0,0 @@
-export enum MessageError {
-    GET_TOKEN_BY_REFLECT = 'Token not found.',
-    GET_USER_BY_REFLECT = 'User not found.',
-}
--- Api/src/server/tools/mail/enum/messageError.ts
+++
@@ -1,5 +0,0 @@
-export enum MessageError {
-    EMAIL_BAD_SYNTAX = 'Email has bad syntax.',
-    EMAIL_IS_TEMPORARY = 'Email is temporary mail.',
-
-}
