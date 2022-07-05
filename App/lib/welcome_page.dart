5d6decebba66edb9694d4b178af82d6919678252
Julien <julien.pavoni@epitech.eu>
2022-07-05 13:44:59 +0200

    Change for the pull request

--- App/lib/welcome_page.dart
+++ App/lib/welcome_page.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/dashboard/Home.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'animation.dart';
 import 'Registration/login.dart';
 import './didactiel/didactiel1.dart';
-import './dashboard/profile.dart';
-import './dashboard/planning.dart';
-import './dashboard/absent.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -105,7 +101,7 @@ class WelcomePage extends StatelessWidget {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => Profile(),
+                          builder: (context) => Login(),
                         ),
                       );
                     }),
