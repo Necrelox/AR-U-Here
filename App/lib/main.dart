f635cd78db9d4709e85518701647a4ea86e62733
Anthony <anthony.goti@epitech.eu>
2022-07-11 15:58:33 +0100

    Add camera and face recognisation

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -1,6 +1,11 @@
 import 'myapp.dart';
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-void main() {
+List<CameraDescription>? cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(const MyApp());
 }
