172c082fac6f69dd7751e7792d431003ce02cc78
Ruby <nicolas.da-silva@epitech.eu>
2022-08-22 00:00:23 +0200

    Revert "#111: delete trailing"
    This reverts commit 9d14f83ffba61e5997338b3ff806a2c013069b28.

--- Api/bin/builder.ts
+++ Api/bin/builder.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 
 const srcAddonOrchestrateurFile: string = path.join(__dirname, `../src/addons/build/Release/Orchestrateur.node`);
 const destAddonOrchestrateurFile: string = path.join(__dirname, `../prod/addons/build/Release/Orchestrateur.node`);
+
 if (!fs.existsSync(__dirname +  `/../prod/addons/build/Release/`)) {
     fs.mkdirSync(__dirname +  `/../prod/addons/`)
     fs.mkdirSync(__dirname +  `/../prod/addons/build/`)
